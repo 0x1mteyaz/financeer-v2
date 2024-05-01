7a17cb6bb51c791b9dc2576fd2d2728325fb7c7f
0x1mteyaz <69307952+0x1mteyaz@users.noreply.github.com>
2024-05-01 23:18:38 +0100

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -4,5 +4,5 @@ $i++;
 $cfg['Servers'][$i]['host']='localhost'; //servers ip
 $cfg['Servers'][$i]['auth_type']= 'config'; //auth type
 $cfg['Servers'][$i]['user']= 'imteyaz';
-$cfg['Servers'][$i]['password']= '1337powerful';
+$cfg['Servers'][$i]['password']= '1';
 ?>
